6431bda8f00e201807b9b3b7b6b02beb8f1b341c
kasper084 <kasper084@gmail.com>
2019-12-01 18:51:41 +0200

    alter revers with lambda

--- src/AlterReversArrays.java
+++ src/AlterReversArrays.java
@@ -1,10 +1,9 @@
 import java.util.Arrays;
-import java.util.Collections;
 
 public class AlterReversArrays {
     public static void main(String[] args) {
         Integer[] arr = {2, 3, 1, 7, 11};
-        Arrays.sort(arr, Collections.reverseOrder());
+        Arrays.sort(arr, (element1, element2) -> element2.compareTo(element1));
         System.out.println(Arrays.toString(arr));
     }
 }
