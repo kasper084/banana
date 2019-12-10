ce2e7906683c994299f81f5c43f2948ab9a7d217
kasper084 <kasper084@gmail.com>
2019-12-10 12:04:55 +0200

    deleted empty lines

--- src/shapes/figures/Circle.java
+++ src/shapes/figures/Circle.java
@@ -35,7 +35,6 @@ public class Circle extends PlaneShape {
                 + getVertexValue();
     }
 
-
     public String getVertexValue() {
         double x = VERTEX.getX();
         double y = VERTEX.getY();
