52dcc266ef8bb1be98237ec3c289fb0839c8ee48
kasper084 <kasper084@gmail.com>
2019-12-20 18:23:14 +0200

    removed unnecessary LocalDate

--- src/library/Date.java
+++ src/library/Date.java
@@ -1,14 +1,11 @@
 package library;
 
-import java.time.LocalDate;
-
 public class Date {
     private int year;
     private int month;
     private int day;
 
     public Date(int year, int month, int day) {
-        LocalDate.of(year, month, day);
         this.year = year;
         this.month = month;
         this.day = day;
